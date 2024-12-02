44f50e19fd8d7e1e2d6d4d3cbd18973a707d8ade
Rasmus Henriksson <rasmus@henriksson.nu>
2024-12-02 11:36:22 +0100

    Created script for generating boilerplate

---
+++ new_day.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+
+if [[ $# -gt 0 ]]; then
+    NEW_DAY=$1
+else
+    LAST_DAY="$(ls src | grep day | cut -c 4 | sort | tail -1)"
+    NEW_DAY=$(($LAST_DAY + 1))
+fi
+
+FILE="src/day${NEW_DAY}.rs"
+
+if [[ -f $FILE ]]; then
+    echo "File already exists"
+    exit 1
+fi
+
+echo "Creating $FILE"
+touch $FILE
+cat >$FILE <<EOL
+pub fn main() {
+    let input = std::fs::read_to_string("input/day${NEW_DAY}.txt").expect("No input");
+    println!("Part 1: {}", part1(&input));
+    // println!("Part 2: {}", part2(&input));
+}
+
+fn part1(input: &str) -> i32 {
+    todo!()
+}
+
+fn part2(input: &str) -> i32 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
+EOL
+
+if [[ -f .session ]]; then
+    SESSION=$(<.session)
+    curl https://adventofcode.com/2024/day/${NEW_DAY}/input --compressed -H "Cookie: session=$SESSION" >input/day${NEW_DAY}.txt
+fi
