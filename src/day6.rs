557952467e9b127611c329de49bb076e3d989864
Rasmus Henriksson <rasmus@henriksson.nu>
2024-12-06 15:39:56 +0100

    WIP: Day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -1,26 +1,33 @@
 use glam::IVec2;
 use itertools::Itertools;
+use rayon::prelude::*;
 
 pub fn main() {
     let input = std::fs::read_to_string("input/day6.txt").expect("No input");
     println!("Part 1: {}", part1(&input));
-    // println!("Part 2: {}", part2(&input));
+    println!("Part 2: {}", part2(&input));
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub enum Direction {
+enum Direction {
     North,
     East,
     South,
     West,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+struct Visited {
+    pub tile: IVec2,
+    pub dir: Direction,
+}
+
+#[derive(Debug, Clone)]
 struct World {
     pub obstacles: Vec<IVec2>,
     pub guard_pos: IVec2,
     pub guard_facing: Direction,
-    pub visited: Vec<IVec2>,
+    pub visited: Vec<Visited>,
     pub dimension: IVec2,
 }
 impl World {
@@ -29,7 +36,10 @@ impl World {
             obstacles,
             guard_pos,
             guard_facing: Direction::North,
-            visited: vec![guard_pos],
+            visited: vec![Visited {
+                tile: guard_pos,
+                dir: Direction::North,
+            }],
             dimension,
         }
     }
@@ -62,40 +72,84 @@ fn dir_to_coord(dir: Direction) -> IVec2 {
     }
 }
 
-fn step(world: &mut World) -> bool {
+fn turn(dir: Direction) -> Direction {
+    match dir {
+        Direction::North => Direction::East,
+        Direction::East => Direction::South,
+        Direction::South => Direction::West,
+        Direction::West => Direction::North,
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum Action {
+    Step,
+    Turn,
+    Done,
+}
+
+fn step(world: &mut World) -> Action {
     let next_step = world.guard_pos + dir_to_coord(world.guard_facing);
     if world.obstacles.contains(&next_step) {
-        world.guard_facing = match world.guard_facing {
-            Direction::North => Direction::East,
-            Direction::East => Direction::South,
-            Direction::South => Direction::West,
-            Direction::West => Direction::North,
-        };
-    } else {
-        if next_step.x > world.dimension.x
-            || next_step.y > world.dimension.y
-            || next_step.x < 0
-            || next_step.y < 0
-        {
-            return false;
-        }
-        world.guard_pos = next_step;
-        world.visited.push(next_step);
+        world.guard_facing = turn(world.guard_facing);
+        return Action::Turn;
     }
-
-    true
+    if next_step.x > world.dimension.x
+        || next_step.y > world.dimension.y
+        || next_step.x < 0
+        || next_step.y < 0
+    {
+        return Action::Done;
+    }
+    world.guard_pos = next_step;
+    world.visited.push(Visited {
+        tile: world.guard_pos,
+        dir: world.guard_facing,
+    });
+    Action::Step
 }
 
 fn part1(input: &str) -> i32 {
     let mut world = parse_world(input);
-
-    while step(&mut world) {}
-
-    world.visited.into_iter().unique().count() as i32
+    while step(&mut world) != Action::Done {}
+    world.visited.into_iter().map(|v| v.tile).unique().count() as i32
 }
 
 fn part2(input: &str) -> i32 {
-    todo!()
+    let mut used_world = parse_world(input);
+    let world = used_world.clone();
+    while step(&mut used_world) != Action::Done {}
+
+    let acc: i32 = used_world.visited[1..]
+        .par_iter()
+        .fold(
+            || 0_i32,
+            |acc, v| {
+                let mut world = world.clone();
+
+                let obstacle = v.tile;
+                world.obstacles.push(obstacle);
+
+                loop {
+                    match step(&mut world) {
+                        Action::Done => return acc,
+                        Action::Step => {
+                            let len = world.visited.len();
+                            if world.visited[0..len - 1].contains(&Visited {
+                                tile: world.guard_pos,
+                                dir: world.guard_facing,
+                            }) {
+                                return acc + 1;
+                            }
+                        }
+                        Action::Turn => {}
+                    }
+                }
+            },
+        )
+        .sum();
+
+    acc
 }
 
 #[cfg(test)]
@@ -144,6 +198,30 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(part1(INPUT), 41);
+    }
+
+    #[test]
+    fn test_part1_multiple_obstacles() {
+        let input = "..................
+.....#............
+.......#.#........
+....#....^#.......
+.........#........";
+        assert_eq!(part1(input), 8);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(INPUT), 6);
+    }
+
+    #[test]
+    fn test_part2_cc() {
+        let input = ".##.
+#..#
+....
+..^.";
+        assert_eq!(part2(input), 0);
     }
 
     const INPUT: &str = "....#.....
